dae00f1b25837ccdb55f0ca22dee9fe6748160b4
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2026-03-25 02:27:09 -0300

    Inicio

---
+++ src/main/java/com/calevin/hodor/HodorApplication.java
@@ -0,0 +1,13 @@
+package com.calevin.hodor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HodorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calevin/hodor/HodorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calevin.hodor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
