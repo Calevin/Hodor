4bc7a91fc142499289854ad9c99ce5ef6872ba52
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2026-03-25 21:41:35 -0300

    Added Testcontainers dependencies

--- src/test/java/com/calevin/hodor/HodorApplicationTests.java
+++ src/test/java/com/calevin/hodor/HodorApplicationTests.java
@@ -2,9 +2,18 @@ package com.calevin.hodor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class HodorApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
 
 	@Test
 	void contextLoads() {
